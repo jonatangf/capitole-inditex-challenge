68eeea6d53e302137f2342f12e793bf490f1f40b
Jonatan García <jntngf@gmail.com>
2025-01-11 13:30:19 +0000

    Improve & extend integration tests

--- src/main/java/org/capitole/inditex/application/mapper/PricePageMapper.java
+++ src/main/java/org/capitole/inditex/application/mapper/PricePageMapper.java
@@ -4,7 +4,7 @@ import org.capitole.inditex.application.model.ApplicationPricePage;
 import org.capitole.inditex.domain.model.PricePage;
 import org.mapstruct.Mapper;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = {PriceMapper.class})
 public interface PricePageMapper {
 
     ApplicationPricePage mapToApplicationPricePage(PricePage pricePage);
--- src/test/java/org/capitole/inditex/integration/SearchPricesIntegrationTest.java
+++ src/test/java/org/capitole/inditex/integration/SearchPricesIntegrationTest.java
@@ -1,15 +1,18 @@
 package org.capitole.inditex.integration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.capitole.inditex.adapter.in.controller.price.model.PriceDTO;
 import org.capitole.inditex.adapter.in.controller.price.model.SearchPriceResponseBody;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.List;
+
+import static java.util.Objects.isNull;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -41,18 +44,33 @@ class SearchPricesIntegrationTest {
                 );
 
         assertThat(searchPriceResponseBody).isNotNull();
-        assertThat(searchPriceResponseBody.getPrices()).isNotEmpty().hasSize(4);
+        List<PriceDTO> prices = searchPriceResponseBody.getPrices();
+        assertThat(prices).isNotEmpty().hasSize(4);
+
+        assertThat(prices.stream().anyMatch(price -> isNull(price.getProductId()) || isNull(price.getBrandId())))
+                .isFalse();
+
+        assertThat(prices.get(0).getStartDate()).isEqualTo("2020-06-13T23:00:00Z");
+        assertThat(prices.get(0).getEndDate()).isEqualTo("2020-12-31T23:59:59Z");
+        assertThat(prices.get(1).getStartDate()).isEqualTo("2020-06-14T14:00:00Z");
+        assertThat(prices.get(1).getEndDate()).isEqualTo("2020-06-14T17:30:00Z");
+        assertThat(prices.get(2).getStartDate()).isEqualTo("2020-06-14T23:00:00Z");
+        assertThat(prices.get(2).getEndDate()).isEqualTo("2020-06-15T10:00:00Z");
+        assertThat(prices.get(3).getStartDate()).isEqualTo("2020-06-15T15:00:00Z");
+        assertThat(prices.get(3).getEndDate()).isEqualTo("2020-12-31T23:59:59Z");
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {
-            "2020-06-14T10:00:00Z",
-            "2020-06-14T16:00:00Z",
-            "2020-06-14T21:00:00Z",
-            "2020-06-15T10:00:00Z",
-            "2020-06-16T21:00:00Z"
+    @CsvSource({
+            "2020-06-14T10:00:00Z,2020-06-13T23:00:00Z,2020-12-31T23:59:59Z",
+            "2020-06-14T16:00:00Z,2020-06-14T14:00:00Z,2020-06-14T17:30:00Z",
+            "2020-06-14T21:00:00Z,2020-06-13T23:00:00Z,2020-12-31T23:59:59Z",
+            "2020-06-15T10:00:00Z,2020-06-14T23:00:00Z,2020-06-15T10:00:00Z",
+            "2020-06-16T21:00:00Z,2020-06-15T15:00:00Z,2020-12-31T23:59:59Z"
     })
-    void shouldReturnPriceForAGivenDate(String dateTime) throws Exception {
+    void shouldReturnPriceForAGivenDate(
+            String dateTime, String expectedStartDate, String expectedEndDate
+    ) throws Exception {
         SearchPriceResponseBody searchPriceResponseBody = objectMapper
                 .readValue(
                         mockMvc.perform(get("/api/prices")
@@ -67,6 +85,9 @@ class SearchPricesIntegrationTest {
                 );
 
         assertThat(searchPriceResponseBody).isNotNull();
-        assertThat(searchPriceResponseBody.getPrices()).isNotEmpty().hasSize(1);
+        List<PriceDTO> prices = searchPriceResponseBody.getPrices();
+        assertThat(prices).isNotEmpty().hasSize(1);
+        assertThat(prices.get(0).getStartDate()).isEqualTo(expectedStartDate);
+        assertThat(prices.get(0).getEndDate()).isEqualTo(expectedEndDate);
     }
 }
